1665d6891e29729bbddd87b17797a66be623629e
dlmyL <916748598@qq.com>
2023-07-04 22:54:35 +0800

    add auth center

---
+++ doran-auth-center/src/main/java/com/dlmyl/doran/auth/center/AuthCenterApplication.java
@@ -0,0 +1,19 @@
+package com.dlmyl.doran.auth.center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 授权服务
+ *
+ * @author dlmyL
+ * @date 2023-07-04
+ */
+@SpringBootApplication
+public class AuthCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthCenterApplication.class, args);
+    }
+
+}
